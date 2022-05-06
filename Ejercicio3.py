903fb0da5fee7272d88d1333d6757c9dd28e6c15
franco <franco@localhost>
2022-05-06 08:27:09 -0300

    Ejercicio 3 completado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -2,11 +2,12 @@ width = int(input("Ingrese el ancho de la figura: "))
 height = int(input("Ingrese el alto de la figura: "))
 character = input("Ingrese el caracter de la figura: ")
 
+
 def rectangle(width, height, character):
     for i in range(height):
         for j in range(width):
             print(character, end="")
         print()
 
-rectangle(width, height, character)
 
+rectangle(width, height, character)
