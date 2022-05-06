555ac9c41124c06e93f7fbe12f54d3380fd0cc78
franco <franco@localhost>
2022-05-06 08:29:20 -0300

    Ejercicio 5 completado

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,9 +1,11 @@
 word = input("Ingrese una palabra: ")
 
+
 def palindrome(word):
     if word == word[::-1]:
         print("La palabra " + word + " es capicua")
     else:
-        print("La palabra " + word + " no es capicua")     
+        print("La palabra " + word + " no es capicua")
+
 
 palindrome(word)
