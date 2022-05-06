92bcd83aedf0883f2b083e97b905ba0de1fef314
franco <franco@localhost>
2022-05-06 08:17:17 -0300

    Ejercicio 2 completado

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,5 +1,6 @@
 year = int(input("Ingrese un año: "))
 
+
 def leapYear(year):
     if year % 4 == 0:
         if year % 100 == 0:
@@ -11,5 +12,6 @@ def leapYear(year):
             print("El año es bisiesto")
     else:
         print("El año no es bisiesto")
+
 
 leapYear(year)
