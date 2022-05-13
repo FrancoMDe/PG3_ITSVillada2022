5bd7718dd3120744626441159dea61b1017e31ab
franco <franco@localhost>
2022-05-13 09:55:33 -0300

    Correcion black

--- Guia 3/Ejercicio6.py
+++ Guia 3/Ejercicio6.py
@@ -1,12 +1,14 @@
 import pymysql
 
-miConexion = pymysql.connect( host='localhost', user= 'jsn', passwd='pepe1234', db='Biblioteca' )
+miConexion = pymysql.connect(
+    host="localhost", user="jsn", passwd="pepe1234", db="Biblioteca"
+)
 cur = miConexion.cursor()
 try:
-    cur.execute( "SELECT * FROM Empleado" )
-    print( cur.fetchall() )
+    cur.execute("SELECT * FROM Empleado")
+    print(cur.fetchall())
 except pymysql.err.ProgrammingError as e:
-    print( "Error en el comando!!!!!")
-    print( e )
+    print("Error en el comando!!!!!")
+    print(e)
 
 miConexion.close()
