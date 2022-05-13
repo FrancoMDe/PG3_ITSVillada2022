db6f2bc6d7276d1768fb841c85a65dd475311276
franco <franco@localhost>
2022-05-13 08:19:41 -0300

    Correciones black

--- Guia 3/Ejercicio3.py
+++ Guia 3/Ejercicio3.py
@@ -1,9 +1,26 @@
-months = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+months = (
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+)
 
 while True:
     try:
-        numero = int(input("Ingrese un numero del 1 al 12, correspondiendo a los meses del año: "))
-        print(f"El mes numero {numero} es el mes de: {months[numero-1]}" )
+        numero = int(
+            input(
+                "Ingrese un numero del 1 al 12, correspondiendo a los meses del año: "
+            )
+        )
+        print(f"El mes numero {numero} es el mes de: {months[numero-1]}")
 
         while True:
             continuar = input("¿Desea continuar? (Si/No): ")
@@ -15,6 +32,6 @@ while True:
                 print("Ingrese una opción válida")
 
     except ValueError:
-        print("No es un número, porfavor ingrese un número") 
+        print("No es un número, porfavor ingrese un número")
     except IndexError:
         print("El número no corresponde a un mes del año")
--- Guia 3/Ejercicio5.py
+++ Guia 3/Ejercicio5.py
@@ -3,4 +3,4 @@ texto.write("Hola, esto es una prueba")
 try:
     texto.write(8)
 except TypeError:
-    print("Error: No se puede escribir un número, solo se permiten strings")    
+    print("Error: No se puede escribir un número, solo se permiten strings")
