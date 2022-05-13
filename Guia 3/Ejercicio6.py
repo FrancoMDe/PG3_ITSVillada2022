65780c3d809df6dad174bfbdcc8431806779ae3c
franco <franco@localhost>
2022-05-13 09:53:30 -0300

    Ejercicio 6 completado

---
+++ Guia 3/Ejercicio6.py
@@ -0,0 +1,12 @@
+import pymysql
+
+miConexion = pymysql.connect( host='localhost', user= 'jsn', passwd='pepe1234', db='Biblioteca' )
+cur = miConexion.cursor()
+try:
+    cur.execute( "SELECT * FROM Empleado" )
+    print( cur.fetchall() )
+except pymysql.err.ProgrammingError as e:
+    print( "Error en el comando!!!!!")
+    print( e )
+
+miConexion.close()
